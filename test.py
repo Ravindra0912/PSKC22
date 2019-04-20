1684f2a43a8ab96cafe406359e38d6945c40f5a9
Ravindra0912 <ravindra.shrivastava.9@gmail.com>
2019-04-20 19:07:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-num_days = []
-start_date = start_date.split()
-end_date = end_date.split()
-sdarr = start_date.split('-')
-edarr = end_date.split('-')
-f_date = date(int(sdarr[0]), int(sdarr[1]), int(sdarr[2]))
-l_date = date(int(edarr[0]), int(edarr[1]), int(edarr[2]))
-delta = l_date - f_date
